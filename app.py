f9e9a5d67b6a3ff05372db0b843f181566879419
grischa1086 <Gregor.Enns@gmx.de>
2026-02-15 20:38:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_ga import GoogleAnalytics
 
 # Dein GA-Tracking-ID (z. B. 'G-XXXXXXXXXX' – erstelle ein Konto auf analytics.google.com)
-ga = GoogleAnalytics('G-DEINE-ID')
+ga = GoogleAnalytics('G-4F63Z1DGEF')
 ga.track()
 
 st.set_page_config(page_title="Unterhaltsrechner 2026 Pro", page_icon="👨‍👧‍👦", layout="wide")
